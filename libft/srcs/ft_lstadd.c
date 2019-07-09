03d86a4d38075d1d1a183a2194ae2acdb7873a0c
Alistair Norman <anorman@c5r10s9.wethinkcode.co.za>
2019-07-09 15:48:24 +0200

    lstadd updated to work for adding to NULL

--- libft/srcs/ft_lstadd.c
+++ libft/srcs/ft_lstadd.c
@@ -6,7 +6,7 @@
 /*   By: anorman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/30 15:18:51 by anorman           #+#    #+#             */
-/*   Updated: 2019/07/09 15:46:56 by anorman          ###   ########.fr       */
+/*   Updated: 2019/07/09 15:48:20 by anorman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,12 @@
 
 void	ft_lstadd(t_list **alst, t_list *new)
 {
-	if (alst && new)
-		new->next = *alst;
-	else if (new)
-		new->next = NULL;
-	*alst = new;
+	if (alst)
+	{
+		if (*alst && new)
+			new->next = *alst;
+		else if (new)
+			new->next = NULL;
+		*alst = new;
+	}
 }

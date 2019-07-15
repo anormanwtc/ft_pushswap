26f882fbab45b0b77050f0b0f7f294671ec6d353
Alistair Norman <anorman@c5r10s6.wethinkcode.co.za>
2019-07-15 14:10:23 +0200

    mem/lstreplace stupidly protected, fixed

--- libft/srcs/ft_lstreplace.c
+++ libft/srcs/ft_lstreplace.c
@@ -6,7 +6,7 @@
 /*   By: anorman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/11 14:34:29 by anorman           #+#    #+#             */
-/*   Updated: 2019/07/14 13:47:01 by anorman          ###   ########.fr       */
+/*   Updated: 2019/07/15 14:09:39 by anorman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_lstreplace(t_list **old, t_list *replace)
 {
-	if (*old)
+	if (old)
 	{
 		ft_lstdel(old);
 		*old = replace;
--- libft/srcs/ft_memreplace.c
+++ libft/srcs/ft_memreplace.c
@@ -6,7 +6,7 @@
 /*   By: anorman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/10 11:29:36 by anorman           #+#    #+#             */
-/*   Updated: 2019/07/14 11:41:15 by anorman          ###   ########.fr       */
+/*   Updated: 2019/07/15 14:09:53 by anorman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 
 void	ft_memreplace(void **old, void *new)
 {
-	free(*old);
-	*old = new;
+	if (old)
+	{
+		free(*old);
+		*old = new;
+	}
 }

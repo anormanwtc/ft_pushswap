616b99ce6914b898b89840698660b0fbeecc3aab
Alistair Norman <anorman@c5r10s6.wethinkcode.co.za>
2019-07-21 14:54:44 +0200

    fixed bracket issue caused by thoughtless norming

--- libft/srcs/ft_can_atoi.c
+++ libft/srcs/ft_can_atoi.c
@@ -6,7 +6,7 @@
 /*   By: anorman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/04 14:09:04 by anorman           #+#    #+#             */
-/*   Updated: 2019/07/21 13:44:05 by anorman          ###   ########.fr       */
+/*   Updated: 2019/07/21 14:26:33 by anorman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ int		ft_can_atoi(char *str)
 	}
 	if (*intmax)
 		return (1 + ft_isspace(str[0]) + (str[i] != '\0') * 2);
-	else if (big || str[i] > 47 && str[i] < 58)
+	else if (big || (str[i] > 47 && str[i] < 58))
 		return (0);
 	return (-1);
 }

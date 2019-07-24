0c50f63808d7948f0eff46adb3fcbee5503eb069
Alistair Norman <anorman@c5r10s6.wethinkcode.co.za>
2019-07-24 16:52:45 +0200

    ft_checker_main.c

--- ft_stackbigsort.c
+++ ft_stackbigsort.c
@@ -6,7 +6,7 @@
 /*   By: anorman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/21 16:00:24 by anorman           #+#    #+#             */
-/*   Updated: 2019/07/24 12:26:57 by anorman          ###   ########.fr       */
+/*   Updated: 2019/07/24 16:51:52 by anorman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,24 +19,21 @@ int				ft_pushplace_a(int *arr, int len, int elem)
 	int min;
 
 	i = 0;
-	min = arr[0];
 	place = 0;
-	if (len == 2 && elem > arr[0] && elem < arr[1])
-		place = 1;
-	else if (len == 2 || (elem > arr[0] && elem < arr[len - 1]))
-		place = 0;
-	else
-		while (i + 1 < len)
+	min = arr[0];
+	while (i + 1 < len)
+	{
+		if (elem > arr[i] && elem < arr[i + 1])
+			return (i + 1);
+		i++;
+		if (arr[i] < min)
 		{
-			if (elem > arr[i] && elem < arr[i + 1])
-				return (i + 1);
-			i++;
-			if (arr[i] < min)
-			{
-				min = arr[i];
-				place = i;
-			}
+			min = arr[i];
+			place = i;
 		}
+	}
+	if (elem < arr[0] && elem > arr[len - 1])
+		place = 0;
 	return (place);
 }
 
@@ -64,20 +61,20 @@ int				ft_pushplace_b(int *arr, int len, int elem)
 			place = i;
 		}
 	}
-	if ((elem > arr[0] && elem < arr[len - 1]))
+	if (elem > arr[0] && elem < arr[len - 1])
 		place = 0;
 	return (place);
 }
 
 static void		st_pa_allordered(t_stk **stack)
 {
-//	int index;
+	int index;
 
 	while (stack[1]->len)
 	{
-//		index = ft_pushplace_a(stack[0]->start, stack[0]->len
-//				, stack[1]->start[0]);
-//		ft_rota_to(stack, index);
+		index = ft_pushplace_a(stack[0]->start, stack[0]->len
+				, stack[1]->start[0]);
+		ft_rota_to(stack, index);
 		ft_stkpush('a', stack);
 		write(1, "pa\n", 3);
 	}
@@ -87,7 +84,7 @@ static void		st_pa_allordered(t_stk **stack)
 /*
 ** Pushes all (remaining) from a to b in order.
 */
-/*
+
 static void		st_pb_all(t_stk **stack)
 {
 	int		index;
@@ -112,24 +109,21 @@ static void		st_pb_all(t_stk **stack)
 		}
 	}
 }
-*/
+
 void			ft_bigsort(t_stk **stacks)
 {
-//	int		opti;
+	int		opti;
 	int		index;
 
-//	opti = (stacks[0]->len / 4);
+	opti = (stacks[0]->len > 200 ? 50 : 2);
 	ft_stkpush('b', stacks);
 	ft_stkpush('b', stacks);
 	write(1, "pb\npb\n", 6);
-	while (stacks[0]->len > 1)// > opti)
+	while (stacks[0]->len > opti)
 	{
 		index = ft_shortest_to_pb(stacks);
 		ft_pbindex_inorder(stacks, index);
 	}
-	index = ft_pushplace_b(stacks[1]->start, stacks[1]->len, 
-			stacks[0]->start[0]);
-	ft_rotb_to(stacks, index);
-//	st_pb_all(stacks);
+	st_pb_all(stacks);
 	st_pa_allordered(stacks);
 }
